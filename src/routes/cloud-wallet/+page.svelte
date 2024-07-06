afdc7f890022b6e099f2ba0b4bc876548b2f2164
Rana Mahad Ahmer <ranamahadahmer@outlook.com>
2024-07-07 02:39:13 +0500

    no

--- src/routes/cloud-wallet/+page.svelte
+++ src/routes/cloud-wallet/+page.svelte
@@ -1,137 +1,137 @@
-<!--<script>-->
-<!--    export let form-->
-<!--</script>-->
-<!--<div-->
-<!--    class="mt-10 pt-10 w-full max-w-xl p-12 mx-auto rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg">-->
-<!--    <form method="POST" action="?/login">-->
-<!--        <div class="flex flex-wrap -mx-3 mb-2">-->
+<script>
+    export let form
+</script>
+<div
+    class="mt-10 pt-10 w-full max-w-xl p-12 mx-auto rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg">
+    <form method="POST" action="?/login">
+        <div class="flex flex-wrap -mx-3 mb-2">
 
-<!--            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">-->
-<!--                <label-->
-<!--                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"-->
-<!--                    for="email"-->
-<!--                >-->
-<!--                    Email-->
-<!--                </label>-->
-<!--                <input-->
-<!--                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"-->
-<!--                    id="email"-->
-<!--                    type="text"-->
-<!--                    placeholder="Enter email"-->
-<!--                    name="email"-->
-<!--                />-->
-<!--            </div>-->
-<!--            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">-->
-<!--                <label-->
-<!--                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"-->
-<!--                    for="password"-->
-<!--                >-->
-<!--                    Password-->
-<!--                </label>-->
-<!--                <input-->
-<!--                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"-->
-<!--                    id="password"-->
-<!--                    type="password"-->
-<!--                    placeholder="Enter Password"-->
-<!--                    name="password"-->
-<!--                />-->
-<!--            </div>-->
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="email"
+                >
+                    Email
+                </label>
+                <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="email"
+                    type="text"
+                    placeholder="Enter email"
+                    name="email"
+                />
+            </div>
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="password"
+                >
+                    Password
+                </label>
+                <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="password"
+                    type="password"
+                    placeholder="Enter Password"
+                    name="password"
+                />
+            </div>
 
-<!--            <button-->
-<!--                type="submit"-->
-<!--                class="bg-yellow-500 hover:bg-blue-700 text-white font-bold mt-5 ml-2 px-2 rounded "-->
-<!--            >-->
-<!--                Login-->
-<!--            </button>-->
-<!--        </div>-->
-<!--    </form>-->
-<!--    {#if form?.success && form?.login}-->
-<!--        <p class="pt-2">Logged In. Your Balance is {form?.data}</p>-->
-<!--    {/if}-->
-<!--</div>-->
+            <button
+                type="submit"
+                class="bg-yellow-500 hover:bg-blue-700 text-white font-bold mt-5 ml-2 px-2 rounded "
+            >
+                Login
+            </button>
+        </div>
+    </form>
+    {#if form?.success && form?.login}
+        <p class="pt-2">Logged In. Your Balance is {form?.data}</p>
+    {/if}
+</div>
 
 
-<!--<div-->
-<!--    class="mt-10 pt-10 w-full max-w-xl p-12 mx-auto rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg"-->
-<!-->-->
-<!--    <form method="POST" action="?/send">-->
-<!--        <div class="flex flex-wrap -mx-3 mb-2">-->
+<div
+    class="mt-10 pt-10 w-full max-w-xl p-12 mx-auto rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg"
+>
+    <form method="POST" action="?/send">
+        <div class="flex flex-wrap -mx-3 mb-2">
 
-<!--            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">-->
-<!--                <label-->
-<!--                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"-->
-<!--                    for="email"-->
-<!--                >-->
-<!--                    Email-->
-<!--                </label>-->
-<!--                <input-->
-<!--                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"-->
-<!--                    id="email"-->
-<!--                    type="text"-->
-<!--                    placeholder="Enter email"-->
-<!--                    name="email"-->
-<!--                />-->
-<!--            </div>-->
-<!--            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">-->
-<!--                <label-->
-<!--                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"-->
-<!--                    for="password"-->
-<!--                >-->
-<!--                    Password-->
-<!--                </label>-->
-<!--                <input-->
-<!--                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"-->
-<!--                    id="password"-->
-<!--                    type="password"-->
-<!--                    placeholder="Enter Password"-->
-<!--                    name="password"-->
-<!--                />-->
-<!--            </div>-->
-<!--            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">-->
-<!--                <label-->
-<!--                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"-->
-<!--                    for="paymail"-->
-<!--                >-->
-<!--                    Paymail-->
-<!--                </label>-->
-<!--                <input-->
-<!--                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"-->
-<!--                    id="paymail"-->
-<!--                    type="text"-->
-<!--                    placeholder="Enter Paymail"-->
-<!--                    name="paymail"-->
-<!--                />-->
-<!--            </div>-->
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="email"
+                >
+                    Email
+                </label>
+                <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="email"
+                    type="text"
+                    placeholder="Enter email"
+                    name="email"
+                />
+            </div>
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="password"
+                >
+                    Password
+                </label>
+                <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="password"
+                    type="password"
+                    placeholder="Enter Password"
+                    name="password"
+                />
+            </div>
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="paymail"
+                >
+                    Paymail
+                </label>
+                <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="paymail"
+                    type="text"
+                    placeholder="Enter Paymail"
+                    name="paymail"
+                />
+            </div>
 
-<!--            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">-->
-<!--                <label-->
-<!--                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"-->
-<!--                    for="ammount"-->
-<!--                >-->
-<!--                    Ammount-->
-<!--                </label>-->
-<!--                <input-->
-<!--                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"-->
-<!--                    id="ammount"-->
-<!--                    type="number"-->
-<!--                    placeholder="Enter Ammount"-->
-<!--                    name="ammount"-->
-<!--                />-->
-<!--            </div>-->
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="ammount"
+                >
+                    Ammount
+                </label>
+                <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="ammount"
+                    type="number"
+                    placeholder="Enter Ammount"
+                    name="ammount"
+                />
+            </div>
 
-<!--            <button-->
-<!--                type="submit"-->
-<!--                class="bg-yellow-500 hover:bg-blue-700 text-white font-bold mt-5 ml-2 px-2 rounded "-->
-<!--            >-->
-<!--                Pay-->
-<!--            </button>-->
-<!--        </div>-->
-<!--    </form>-->
-<!--    {#if form?.success && form?.send}-->
-<!--        <p class="pt-2">Transaction Successful: <a style="color: blue"-->
-<!--                                                   target="_blank"-->
-<!--                                                   href='https://whatsonchain.com/tx/{form?.tx}'>Transaction Link!</a>-->
-<!--        </p>-->
-<!--    {/if}-->
+            <button
+                type="submit"
+                class="bg-yellow-500 hover:bg-blue-700 text-white font-bold mt-5 ml-2 px-2 rounded "
+            >
+                Pay
+            </button>
+        </div>
+    </form>
+    {#if form?.success && form?.send}
+        <p class="pt-2">Transaction Successful: <a style="color: blue"
+                                                   target="_blank"
+                                                   href='https://whatsonchain.com/tx/{form?.tx}'>Transaction Link!</a>
+        </p>
+    {/if}
 
-<!--</div>-->
+</div>
