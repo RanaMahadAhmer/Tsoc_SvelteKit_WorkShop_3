28179c05b8f5d1bf2b2b4c28b5e9e80776d97e25
Rana Mahad Ahmer <ranamahadahmer@outlook.com>
2024-07-06 16:15:24 +0500

    Update Functionality Added

--- src/routes/profiles/edit/[id]/+page.server.ts
+++ src/routes/profiles/edit/[id]/+page.server.ts
@@ -5,3 +5,32 @@ export async function load({ params, url }) {
     const email = url.searchParams.get('email');
     return { props: { id, name, email } };
 }
+
+
+// import { error } from '@sveltejs/kit';
+import { createPool, sql } from '@vercel/postgres'
+import { POSTGRES_URL } from '$env/static/private'
+
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+
+   update: async ({ request }) => {
+     const data = await request.formData();
+     const db = createPool({ connectionString: POSTGRES_URL })
+     const client = await db.connect();
+
+     const email = data.get('email');
+	 const name = data.get('name');
+     const id = data.get('id');
+
+
+     const updateUser = await client.sql`
+     UPDATE names
+     SET email = ${email}, name = ${name}
+     WHERE id= ${id};`
+	 	return { success: true };
+	 },
+};
+
+
