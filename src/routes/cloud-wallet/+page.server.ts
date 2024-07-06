80cfd8c0faa5eb906d7d656913dbac4c50210f34
Rana Mahad Ahmer <ranamahadahmer@outlook.com>
2024-07-07 02:26:08 +0500

    no

--- src/routes/cloud-wallet/+page.server.ts
+++ src/routes/cloud-wallet/+page.server.ts
@@ -1,28 +1,28 @@
-/** @type {import('./$types').Actions} */
-import NeucronSDK from "neucron-sdk";
-
-
-export const actions = {
-    login: async ({request}) => {
-        const [data] = await Promise.all([request.formData()]);
-        const id = data.get('email');
-        const password = data.get('password');
-        console.log(id);
-        console.log(password);
-
-        const neucron = new NeucronSDK();
-
-        const authModule = neucron.authentication;
-        const walletModule = neucron.wallet;
-
-        const loginResponse = await authModule.login({email: id, password: password});
-        console.log(loginResponse);
-
-        const DefaultWalletBalance = await walletModule.getWalletBalance({});
-        console.log(DefaultWalletBalance);
-
-        return {success: true, login: true, data: DefaultWalletBalance['data']['balance']['summary']};
-    },
+///** @type {import('./$types').Actions} */
+//import NeucronSDK from "neucron-sdk";
+//
+//
+//export const actions = {
+//    login: async ({request}) => {
+//        const [data] = await Promise.all([request.formData()]);
+//        const id = data.get('email');
+//        const password = data.get('password');
+//        console.log(id);
+//        console.log(password);
+//
+//        const neucron = new NeucronSDK();
+//
+//        const authModule = neucron.authentication;
+//        const walletModule = neucron.wallet;
+//
+//        const loginResponse = await authModule.login({email: id, password: password});
+//        console.log(loginResponse);
+//
+//        const DefaultWalletBalance = await walletModule.getWalletBalance({});
+//        console.log(DefaultWalletBalance);
+//
+//        return {success: true, login: true, data: DefaultWalletBalance['data']['balance']['summary']};
+//    },
 //    send: async ({request}) => {
 //        const [data] = await Promise.all([request.formData()]);
 //        const id = data.get('email');
@@ -62,8 +62,8 @@ export const actions = {
 //
 //        return {success: true, send: true, tx: payResponse['data']['txid']};
 //    }
-};
-  
-  
-  
-  
+//};
+//
+//  
+//
+//
